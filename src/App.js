ad0f0aa8536a828782f72349dbd145111571da4f
Tahere <tahere.avvali@gmail.com>
2021-06-07 22:39:46 +0430

    test git and change text in App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App" class="p-6 items-center justify-center">
-        <h1 class="text-blue-400 font-extrabold">Hello World!</h1>
+        <h1 class="text-blue-400 font-extrabold">Start ToDo List project</h1>
         <p class="tracking-widest">This is my first React App.</p>
     </div>
   );
